a056022cdd04424602d2cee6d111d0a6d1ee9d73
Marissa Nolan <ritz1484@cs.com>
2021-01-18 20:30:18 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  
+  array.length.times do |something|
+    new_array.push(array[something]**2)
+  end
+  return new_array
 end
+
+square_array([1,2,3])
+
+# takes each element in an array
+# squares that element
+# retrns new array containing the squared elements
