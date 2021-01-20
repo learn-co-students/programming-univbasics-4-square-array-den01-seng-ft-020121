e6bb17594f835338af4959598a254de2aa96ab84
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-20 20:20:02 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+
+new_numbers = Array.new(numbers.length)
+i = 0
+while i < numbers.length
+  new_numbers.push[i] = numbers[i] **2
+  i += 1
+end
+new_numbers
 end
